05cfbbc58b3dca51320d18082e212898a84d697b
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-04-12 14:45:39 +0200

    adding code pyramids

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -14,3 +14,24 @@ end
 
 def full_pyramid
     
+
+### CHEAT CODE 
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25)"
+print "> "
+user_number=gets.chomp.to_i
+while 0 > user_number or user_number >25 or user_number.to_i==0
+  puts "Merci de choisir un nombre entre 1 et 25"
+  print "> "
+user_number=gets.chomp.to_i
+end 
+puts "Voici la pyramide :"
+user_number.times do |i| #on crée la pyramide
+  (user_number-i-1).times do #vs. prog 15 on remplace les # par des 0 puis on ajoute un #
+    print " "
+  end
+  (i+1).times do
+    print "#"
+  end
+  puts ""
+end
