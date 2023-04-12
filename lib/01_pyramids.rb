7682e138458f4a9bcef42d0421a7807da9645c59
katamaric <katherinehcao@gmail.com>
2023-04-12 16:12:59 +0200

    test1 pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -5,33 +5,33 @@ def half_pyramid
 
   puts "Great ! Here's the pyramid : "
 
-  x = 1
-  while x <= levels
-    puts ("#" * x).rjust(levels)
-    x += 1
-  end
-end
-
-def full_pyramid
-    
-
-### CHEAT CODE 
-
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25)"
-print "> "
-user_number=gets.chomp.to_i
-while 0 > user_number or user_number >25 or user_number.to_i==0
-  puts "Merci de choisir un nombre entre 1 et 25"
-  print "> "
-user_number=gets.chomp.to_i
-end 
-puts "Voici la pyramide :"
-user_number.times do |i| #on crée la pyramide
-  (user_number-i-1).times do #vs. prog 15 on remplace les # par des 0 puis on ajoute un #
+  levels.times do |i| #on crée la pyramide
+    (levels-i-1).times do #vs. prog 15 on remplace les # par des 0 puis on ajoute un #
     print " "
   end
   (i+1).times do
-    print "#"
+    print "#" 
   end
-  puts ""
+    puts ""
+  end
+end
+
+
+def full_pyramid
+    puts "Hi, welcome to my super pyramid ! How many levels would you like ?"
+    print ">"
+    levels = gets.chomp.to_i
+
+    puts "Great ! Here's the pyramid : "
+
+    x = 1
+    1.upto(levels) do
+        print ' ' * levels
+        print '#' * (2 * x - 1)
+
+        print "\n"
+
+        levels -= 1
+        x += 1
+    end
 end
